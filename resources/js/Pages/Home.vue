fa6247e4b9396d04e0fe99b04de1fe30016e9703
Dana Tal <dana-tal@012.net.il>
2024-12-29 08:45:52 +0200

    Home addition

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,14 @@
 <template> <!-- comment -->
             <div>
                 <div class="row"><div class="col"><h1>Welcome</h1></div></div>
-                <div v-if="username=== null " class="row"><div class="col"><p>Please login or register </p></div></div>
+                <div v-if="username=== null ">
+                    <div  class="row">
+                        <div class="col"><p>Please login or register </p></div>
+                    </div>
+                    <div  class="row">
+                        <div class="col"><p>You can login with: test, 123456 </p></div>
+                    </div>
+                </div>
                 <div v-else class="row"><div class="col"><p>Welcome back {{ username }}</p></div></div>
             </div>
 </template>
